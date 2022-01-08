563e0e17e949160d53689f385fa13b270bd70466
aospiridonov <aospiridonov@gmail.com>
2022-01-08 15:21:29 +0300

    add loading image, add species and gender

--- lib/ui/widgets/custom_list_tile.dart
+++ lib/ui/widgets/custom_list_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rick_and_morty/data/models/character.dart';
 
@@ -7,10 +8,94 @@ class CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        title: Text(
-      result.name,
-      style: const TextStyle(color: Colors.white),
-    ));
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 7,
+        color: const Color.fromRGBO(86, 86, 86, 0.8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CachedNetworkImage(
+              imageUrl: result.image,
+              progressIndicatorBuilder: (context, url, downloadProgress) =>
+                  CircularProgressIndicator(
+                value: downloadProgress.progress,
+                color: Colors.grey,
+              ),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.9,
+                      child: Text(
+                        result.name,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.bodyText2,
+                      )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width / 2,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //Species column
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Species',
+                                style: Theme.of(context).textTheme.caption,
+                              ),
+                              const SizedBox(
+                                height: 2,
+                              ),
+                              Text(
+                                result.species,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context).textTheme.bodyText1,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            //Gender column
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Gender',
+                                style: Theme.of(context).textTheme.caption,
+                              ),
+                              const SizedBox(
+                                height: 2,
+                              ),
+                              Text(
+                                result.gender,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context).textTheme.bodyText1,
+                              ),
+                            ],
+                          )
+                        ],
+                      )),
+                  const SizedBox(
+                    height: 10,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
